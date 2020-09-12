cf1364cea7fa265fae5d3cbb6661d9a917b73f81
Madison Werner <mwerner640@gmail.com>
2020-09-12 09:09:37 -0400

    No auth login functional

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
+import App from './App'
 import configureStore from './store-config';
-import routes from './routes';
 
-injectTapEventPlugin();
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-            {routes}
+        <App />
     </Provider>,
     document.getElementById('root')
 );
--- client/src/store-config.tsx
+++ client/src/store-config.tsx
@@ -2,17 +2,7 @@ import { createStore } from 'redux';
 import comboReducer from './reducers/combo-reducer';
 
 export default function configureStore() {
-    const store = createStore(
-        comboReducer
-    );
-
-    if (module.hot) {
-        module.hot.accept('./reducers', () => {
-            const nextRootReducer = require('./reducers/combo-reducer').default;
-
-            store.replaceReducer(nextRootReducer);
-        });
-    }
+    const store = createStore(comboReducer);
 
     return store;
 }
