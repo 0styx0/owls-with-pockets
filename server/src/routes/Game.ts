e82cc09d41b4200d8776ae4ce855335b75e925a2
David Meiseles <me@davidm.me>
2020-09-12 15:44:04 -0400

    sessions might work

---
+++ server/src/routes/Game.ts
@@ -0,0 +1,40 @@
+import { Request, Response, Router } from 'express';
+import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+
+import IGame from '../classes/IGame';
+import Game from '../classes/Game';
+// import IUser from '../classes/IUser';
+// import User from '../classes/User';
+
+// Init shared
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.send('game get');
+});
+
+router.post('/create', async (req: Request, res: Response) => {
+
+    const gameInfo = req.query as any as IGame;
+
+    if (!gameInfo) {
+        return res.status(BAD_REQUEST).json({});
+    }
+
+    if (!req.session!.user) {
+      console.log('no session user');
+      return res.status(BAD_REQUEST).json({});
+    }
+
+    const game = new Game();
+
+    try {
+      await game.create(gameInfo.name, gameInfo.data, req.session!.user.id);
+    } catch (e) {
+      return res.status(BAD_REQUEST).end();
+    }
+
+    return res.status(CREATED).end();
+});
+
+export default router;
