c80b6fd44275ffbd8c45661d4eb53f4c4ef71c0e
David Meiseles <me@davidm.me>
2020-09-12 21:22:36 -0400

    fixed header responsiveness and funcionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import LoginContainer from './pages/LoginContainer';
-import Header from './pages/Header';
+// import Header from './pages/Header';
 import Signup from './pages/Signup';
+import "./pages/style.css";
 
 
 function App() {
   return (
-    <Router>
-	  <Header />
-      <div>
-        <ul>
-          <li>
-            <Link to="/login">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-            </li>
-        </ul>
-        <Route path="/login" component={LoginContainer} />
-        <Route path="/signup" component={Signup} />
-      </div>
-    </Router>
+    <div>
+      <Router>
+          <div>
+            <h1>GamePocket</h1>
+            <p id="heading">Enjoy some classic board games! How high will you rank?</p>
+
+            <nav className="menu-bar">
+              <input type="button" className="button" value="Featured Games" />
+              <input type="button" className="button" value="All Games" />
+              <input type="button" className="button" value="Leaderboard" />
+              <input type="button" className="button" value="Create Game" />
+              <input type="button" className="button" value="My Profile" / >
+              <Link to="/signup"><input type="button" className="button" value="Sign Up" /></Link>
+              <Link to="/login"><input type="button" className="button" value="Log In" /></Link>
+            </nav>
+          </div>
+
+          <Route path="/login" component={LoginContainer} />
+          <Route path="/signup" component={Signup} />
+      </Router>
+    </div>
   );
 }
 
