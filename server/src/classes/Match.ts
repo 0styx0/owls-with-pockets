d6e8753ed461fba7cfa0b7f33c5d925afaae5cc8
David Meiseles <me@davidm.me>
2020-09-13 00:25:39 -0400

    added some console logs

--- server/src/classes/Match.ts
+++ server/src/classes/Match.ts
@@ -15,6 +15,7 @@ export default class Match {
 
     static async findMatch(gameId: string, userId: string) {
 
+        console.log('findMatch');
         const matchQuery = 'SELECT person1, id FROM matches WHERE (game_id = ? AND person1 != ?) AND (person2 IS NULL)';
 
         const [rows] = await (await connection as any).execute(
@@ -23,14 +24,17 @@ export default class Match {
         );
 
         if (!rows) {
+            console.log('\tno matches fonud');
             return false;
         }
 
+        console.log('\tyes matches fonud');
         return rows[0];
     }
 
     static async createWaitingMatch(gameId: string, userId: string) {
 
+        console.log('createWaitingMatch');
         const noDupsQuery = 'SELECT id FROM matches WHERE game_id = ? AND person1 = ? and person2 IS NULL';
 
         const [rows] = await (await connection as any).execute(
@@ -39,10 +43,12 @@ export default class Match {
         );
 
         if (rows) {
+            console.log('\talready waiting');
             return false; // already watinig on a match. dont re-add
         }
 
         const creationQuery = 'INSERT INTO matches (person1, game_id) VALUES(?, ?)';
+        console.log('\tinserting row');
 
         return await (await (connection as any)).execute(
             creationQuery,
@@ -52,6 +58,7 @@ export default class Match {
 
     static async foundMatch(gameId: string, userId: string) {
 
+        console.log('foundMatch');
         const foundMatchQuery = 'SELECT person1, person2, id FROM matches WHERE game_id = ? AND (person1 = ? OR person2 = ?) AND person1 IS NOT NULL AND person2 IS NOT NULL AND matched != ?';
 
         const [rows] = await (await connection as any).execute(
@@ -61,6 +68,7 @@ export default class Match {
 
         if (rows) {
 
+            console.log('\tfound match');
             const matched = 'UPDATE matches SET matched = ? WHERE id = ?';
             await (await (connection as any)).execute(
                 matched,
@@ -73,6 +81,7 @@ export default class Match {
 
     static async matchUsers(matchId: string, userId: string) {
 
+        console.log('matchUsers');
         const matchQuery = 'UPDATE matches SET person2 = ? WHERE id = ?';
 
         return await (await (connection as any)).execute(
