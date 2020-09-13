16fe5d75cf324cb368f21676cfad059ffe27ff92
Paul Tilley <t1000.pixlark@gmail.com>
2020-09-12 21:35:12 -0400

    game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import LoginContainer from './pages/LoginContainer';
+import { GameContainer } from './pages/GameContainer';
 
 function App() {
   return (
@@ -10,8 +11,12 @@ function App() {
           <li>
             <Link to="/login">Sign In</Link>
           </li>
+          <li>
+            <Link to="/game">Play</Link>
+          </li>
         </ul>
         <Route path="/login" component={LoginContainer} />
+        <Route path="/game" component={GameContainer} />
       </div>
     </Router>
   );
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route } from 'react-router';
 import { HomeContainer } from './pages/HomeContainer';
 import LoginContainer from './pages/LoginContainer';
+import { GameContainer } from './pages/GameContainer';
 
 export default (
     <Route path="/" component={HomeContainer}>
         <Route path="/login" component={LoginContainer} />
+        <Route path="/game" component={GameContainer} />
     </Route>
 );
